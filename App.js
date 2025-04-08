9aca70d76d29cfca1d1ede32b4457d77e3227199
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 10:48:19 -0500

    refactor navigation

--- App.js
+++ App.js
@@ -1,17 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Welcome from "./app/features/welcome/screens/Welcome";
-import { FIREBASE_AUTH } from "./FirebaseConfig";
-import React, { useState, useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { useFonts } from "expo-font";
-import Login from "./app/features/welcome/screens/Login";
-import HomeScreen from "./app/features/welcome/screens/HomeScreen";
-import SignupNavigator from "./app/infraestructure/navigation/SignupNavigator";
-
-const Stack = createNativeStackNavigator();
+import Navigation from "./app/infraestructure/Navigation";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,39 +9,13 @@ export default function App() {
     "AbhayaLibre-SemiBold": require("./assets/fonts/AbhayaLibre-SemiBold.ttf"),
   });
 
-  const [user, setUser] = useState(null);
-  const [initializing, setInitializing] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
-      setUser(currentUser);
-      if (initializing) setInitializing(false);
-    });
-    return unsubscribe;
-  }, []);
-
   if (!fontsLoaded || initializing) {
     return null;
   }
 
   return (
-    <NavigationContainer>
-      {user ? (
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={(props) => <HomeScreen {...props} user={user} />}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-          <Stack.Screen name="SignUp" component={SignupNavigator} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      )}
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <>
+      <Navigation />
+    </>
   );
 }
