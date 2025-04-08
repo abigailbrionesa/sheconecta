41ed97bc9dbee40bac7423b5413959d03cecd8cd
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-07 22:07:59 -0500

    add login

---
+++ app/features/welcome/screens/Login.js
@@ -0,0 +1,54 @@
+import { View, ImageBackground } from "react-native";
+import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+const Login = () => {
+
+  return (
+    <ImageBackground
+      source={require("../../../../assets/background.png")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text>Login</Text>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      marginTop: 100,
+    },
+    h1: {
+      fontSize: 50,
+      color: "white",
+      textAlign: "center",
+      fontFamily: "AbhayaLibre-ExtraBold",
+    },
+    h3: {
+      fontSize: 20,
+      textTransform: "uppercase",
+      color: "white",
+      letterSpacing:2,
+      textAlign: "center",
+      fontFamily: "AbhayaLibre-Bold",
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: "#ccc",
+      padding: 10,
+      marginBottom: 10,
+      borderRadius: 5,
+    },
+    spacer: {
+      height: 10,
+    },
+    background: {
+      width: "100%",
+      height: "100%",
+}});
+  
