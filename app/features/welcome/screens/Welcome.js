d15143aaf4ab1f9e3af7ecd81563722554845519
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 16:52:55 -0500

    add aesthetic button

--- app/features/welcome/screens/Welcome.js
+++ app/features/welcome/screens/Welcome.js
@@ -3,27 +3,26 @@ import { View, Button } from "react-native";
 import LoginHeader from "../components/LoginHeader";
 import { backgroundStyle } from "../../../utils/backgroundStyle";
 import { ImageBackground } from "react-native";
-
+import Button1 from "../components/Button1";
 const Welcome = () => {
   const navigation = useNavigation();
   return (
     <ImageBackground
-    source={require("../../../../assets/background.png")}
-    style={backgroundStyle.background}
-  >   
+      source={require("../../../../assets/background.png")}
+      style={backgroundStyle.background}
+    >
       <View style={{ padding: 20, marginTop: 100 }}>
         <LoginHeader />
-
-        <Button
-          title="CREAR CUENTA"
-          onPress={() => navigation.navigate("SignUp")}
-        />
-        <Button
-          title="INICIAR SESIÓN"
-          onPress={() => navigation.navigate("Login")}
-        />
+        <View style={{ gap: 15 }}>
+          <Button1 onPress={() => navigation.navigate("SignUp")}>
+            Crear Cuenta
+          </Button1>
+          <Button1 onPress={() => navigation.navigate("Login")}>
+            Iniciar Sesión
+          </Button1>
+        </View>
       </View>
-      </ImageBackground>
+    </ImageBackground>
   );
 };
 
