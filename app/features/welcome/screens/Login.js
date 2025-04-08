d8201a7f5e3823ba0488425a2a82932352dafe3c
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 06:35:16 -0500

    email and password for login

--- app/features/welcome/screens/Login.js
+++ app/features/welcome/screens/Login.js
@@ -1,15 +1,75 @@
-import { View, ImageBackground } from "react-native";
-import { Text } from "react-native";
-import backgroundStyle from "../../../utils/backgroundStyle";
-
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  TextInput,
+  View,
+  Text,
+  ImageBackground,
+} from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_AUTH } from "../../../../FirebaseConfig";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { uiStyle } from "../../../utils/uiStyle";
+import { fontStyle } from "../../../utils/fontStyle";
+import { backgroundStyle } from "../../../utils/backgroundStyle";
 const Login = () => {
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const auth = FIREBASE_AUTH;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login successful:", response);
+    } catch (error) {
+      console.error("Login error:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <ImageBackground
       source={require("../../../../assets/background.png")}
       style={backgroundStyle.background}
     >
       <View style={{ padding: 20, marginTop: 100 }}>
-        <Text>Login</Text>
+        <Text style={fontStyle.h1}>Iniciar Sesión</Text>
+
+        <TextInput
+          style={uiStyle.input}
+          value={email}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={uiStyle.input}
+          secureTextEntry
+          value={password}
+          placeholder="Contraseña"
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+        />
+
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <Button title="Iniciar Sesión" onPress={signIn} />
+        )}
+
+        <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+          <Text>
+            ¿No tienes cuenta? <Text>Crea una</Text>
+          </Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
