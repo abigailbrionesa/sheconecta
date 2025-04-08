fe00cb559810bfcf4810604c823b1975104e4280
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 15:39:30 -0500

    new navigator

--- app/infraestructure/OpportunitiesNavigator.js
+++ app/infraestructure/OpportunitiesNavigator.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FeedOfPosts from '../features/opportunities/screens/FeedOfPosts';
 import CreatePost from '../features/opportunities/screens/CreatePost'; 
-import FeedOfPosts from '../features/opportunities/screens/FeedOfPosts';
-import CreatePost from '../features/opportunities/screens/CreatePost';
 
 const Stack = createNativeStackNavigator();
 
