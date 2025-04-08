aec6cb02fadb3e782a1612d7a0102b57eeb260a0
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 10:24:54 -0500

    signupstep6

---
+++ app/features/welcome/screens/SignupStep6.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { View, Button, Image, Text, Alert } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { uiStyle } from "../../../utils/uiStyle";
+
+const SignupStep6 = ({ navigation }) => {
+  const route = useRoute();
+  const [profilePicture, setProfilePicture] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    birthDate,
+    role,
+    experience,
+    city,
+    university,
+    career,
+    instagram,
+    linkedin,
+  } = route.params || {};
+
+
+  const handleContinue = () => {
+    const image = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    console.log( email,
+      password,
+      firstName,
+      lastName,
+      birthDate,
+      role,
+      experience,
+      city,
+      university,
+      career,
+      instagram,
+      linkedin, "lol")
+    navigation.navigate("SignupStep7", {
+      email,
+      password,
+      firstName,
+      lastName,
+      birthDate,
+      role,
+      city,
+      university,
+      career,
+      experience,
+      instagram: instagram || "", 
+      linkedin: linkedin || "",
+      image,
+    });
+  };
+  
+
+  return (
+    <View style={uiStyle.container}>
+      <Text>Step 6: Profile Picture (Optional)</Text>
+
+      {profilePicture ? (
+        <Image
+          source={{ uri: profilePicture }}
+          style={{ width: 100, height: 100, borderRadius: 50 }}
+        />
+      ) : (
+        <Text>No Profile Picture</Text>
+      )}
+
+      <Button title="Continue" onPress={handleContinue} />
+
+    </View>
+  );
+};
+
+export default SignupStep6;
