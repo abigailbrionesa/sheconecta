fef0002d9a65c746ded648ae4beaae9f88d5c7e1
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 06:35:32 -0500

    more styles

---
+++ app/utils/uiStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const uiStyle = StyleSheet.create({
+    input: {
+      borderWidth: 1,
+      borderColor: "#ccc",
+      padding: 10,
+      marginBottom: 10,
+      borderRadius: 5,
+    },
+    container: {
+      padding: 20,
+      margin: 20,
+    },
+});
