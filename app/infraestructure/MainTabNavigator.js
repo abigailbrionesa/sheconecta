edc96a980e59c1665d7e4977da29c105aa1c63b6
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 10:49:51 -0500

    refactor navigation

---
+++ app/infraestructure/MainTabNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Chat from '../features/chat/screens/Chat';
+import Conectar from '../features/connect/screens/Conectar';
+import Oportunidades from '../features/opportunities/screens/Oportunidades';
+import Perfil from '../features/account/screens/Perfil';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainTabNavigator() {
+  return (
+    <Tab.Navigator initialRouteName="Oportunidades" screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Oportunidades" component={Oportunidades} />
+      <Tab.Screen name="Conectar" component={Conectar} />
+      <Tab.Screen name="Chat" component={Chat} />
+      <Tab.Screen name="Perfil" component={Perfil} />
+    </Tab.Navigator>
+  );
+}
