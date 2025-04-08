e07e44d3f1268634432707517888f5343bb7e649
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-07 21:25:40 -0500

    extra styles

---
+++ app/utils/buttonStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const buttonStyles = StyleSheet.create({
+    button1: {
+      backgroundColor: "#FF6F61",
+      borderRadius: 5,
+      padding: 10,
+      fontSize:50,
+      marginBottom: 10,
+    },
+    button2: {
+      backgroundColor: "#6F61FF",
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10,
+    },
+});
+  
