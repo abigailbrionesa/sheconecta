3db0b1a4991f69b0ac736283203a5391507be110
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 10:49:37 -0500

    new screens

---
+++ app/features/opportunities/screens/Oportunidades.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Oportunidades() {
+  return (
+    <View>
+      <Text>Pantalla de Oportunidades</Text>
+    </View>
+  );
+}
