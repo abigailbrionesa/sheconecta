647645e298a1ea5f1e3a9b2f482be801a5c9689f
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 16:41:24 -0500

    refactor color

--- app/utils/fontStyle.js
+++ app/utils/fontStyle.js
@@ -3,26 +3,26 @@ import { StyleSheet } from "react-native";
 export const fontStyle = StyleSheet.create({
     h1: {
       fontSize: 50,
-      color: "white",
       textAlign: "center",
       fontFamily: "AbhayaLibre-ExtraBold",
     },
     h2: {
       fontSize: 35,
-      color: "white",
       textAlign: "center",
       fontFamily: "AbhayaLibre-ExtraBold",
     },
     h3: {
       fontSize: 20,
       textTransform: "uppercase",
-      color: "white",
       letterSpacing:2,
+      fontFamily: "AbhayaLibre-Bold",
+    },
+    h4: {
+      fontSize: 18,
       fontFamily: "AbhayaLibre-Bold",
     },
     p: {
       fontSize: 16,
-      color: "white",
       textAlign: "justify",
       fontFamily: "AbhayaLibre-Medium",
     },
@@ -30,6 +30,18 @@ export const fontStyle = StyleSheet.create({
       fontSize: 12,
       textTransform: "uppercase",
       fontFamily: "AbhayaLibre-ExtraBold",
+    },
+    light: {
+      color:"#faf5f3"
+    },
+    dark: {
+      color: "#6391d0"
+    },
+    gray: {
+      color: "#888888"
+    },
+    pink: {
+      color: "#8d5d9f"
     },
 });
   
