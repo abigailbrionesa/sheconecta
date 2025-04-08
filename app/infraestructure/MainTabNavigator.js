34b7129ee2634b6d13b470ad2cb6a4d6126c293e
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-08 15:39:24 -0500

    new navigator

--- app/infraestructure/MainTabNavigator.js
+++ app/infraestructure/MainTabNavigator.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Chat from '../features/chat/screens/Chat';
 import Conectar from '../features/connect/screens/Conectar';
-import Oportunidades from '../features/opportunities/screens/Oportunidades';
 import Perfil from '../features/account/screens/Perfil';
+import { OpportunitiesNavigator } from './OpportunitiesNavigator';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function MainTabNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Perfil" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Oportunidades" component={Oportunidades} />
+    <Tab.Navigator initialRouteName="Oportunidades" screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Oportunidades" component={OpportunitiesNavigator} />
       <Tab.Screen name="Conectar" component={Conectar} />
       <Tab.Screen name="Chat" component={Chat} />
       <Tab.Screen name="Perfil" component={Perfil} />
