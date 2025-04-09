f4aeef6e66eaaf3d0d21782914d416d710edfb85
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 07:39:13 -0500

    refactor

---
+++ app/features/opportunities/components/PostTypePicker.js
@@ -0,0 +1,37 @@
+import RNPickerSelect from "react-native-picker-select";
+import React from "react";
+
+const PostTypePicker = ({ type, setType }) => {
+  return (
+    <RNPickerSelect
+      onValueChange={setType}
+      placeholder={{
+        label: "(Experiencia, Beca, Curso, Proyecto)",
+        value: null,
+      }}
+      style={{
+        inputIOS: {
+          backgroundColor: "rgba(255, 255, 255, 0.7)",
+          borderRadius: 10,
+        },
+        inputAndroid: {
+          backgroundColor: "rgba(255, 255, 255, 0.7)",
+          borderRadius: 10,
+        },
+        placeholder: {
+          fontSize: 17,
+          color: "gray",
+        },
+      }}
+      value={type}
+      items={[
+        { label: "Experiencia", value: "experiencia" },
+        { label: "Beca", value: "beca" },
+        { label: "Curso", value: "curso" },
+        { label: "Proyecto", value: "proyecto" },
+      ]}
+    />
+  );
+};
+
+export default PostTypePicker;
