4d29673c64ff0ab439e517be8831467480a80add
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 14:43:17 -0500

    merge conflicts

--- app/features/welcome/screens/SignupStep7.js
+++ app/features/welcome/screens/SignupStep7.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Image, Animated } from "react-native";
-=======
 import React, { useState } from "react";
 import { View, TextInput, Button, Alert } from "react-native";
->>>>>>> main
 import { useRoute } from "@react-navigation/native";
 import { uiStyle } from "../../../utils/uiStyle";
 import { Text } from "react-native";
@@ -12,15 +7,6 @@ import { backgroundStyle } from "../../../utils/backgroundStyle";
 import { ImageBackground } from "react-native";
 import NextButton from "../components/NextButton";
 import GoBackButton from "../components/GoBackButton";
-<<<<<<< HEAD
-
-const SignupStep7 = ({ navigation }) => {
-  const route = useRoute();
-  const areas = ["Ciencia", "Tecnología", "Ingeniería", "Matemática"];
-  const [selectedAreas, setSelectedAreas] = useState([]);
-  const fadeAnim = new Animated.Value(0);
-  
-=======
 import { fontStyle } from "../../../utils/fontStyle";
 import { ScrollView } from "react-native";
 
@@ -29,7 +15,6 @@ const SignupStep7 = ({ navigation }) => {
   const [instagram, setInstagram] = useState("");
   const [linkedin, setLinkedin] = useState("");
 
->>>>>>> main
   const {
     email,
     password,
@@ -37,32 +22,6 @@ const SignupStep7 = ({ navigation }) => {
     lastName,
     age,
     role,
-<<<<<<< HEAD
-    city,
-    university,
-    career,
-    experience,
-  } = route.params;
-
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 800,
-      useNativeDriver: true,
-    }).start();
-  }, []);
-
-  const handleAreaSelect = (area) => {
-    setSelectedAreas((prevAreas) => {
-      if (prevAreas.includes(area)) {
-        return prevAreas.filter((item) => item !== area);
-      } else {
-        return [...prevAreas, area];
-      }
-    });
-  };
-
-=======
     departamento,
     provincia,
     university,
@@ -72,7 +31,6 @@ const SignupStep7 = ({ navigation }) => {
 
   } = route.params;
 
->>>>>>> main
   const handleContinue = () => {
     navigation.navigate("SignupStep8", {
       email,
@@ -86,13 +44,9 @@ const SignupStep7 = ({ navigation }) => {
       university,
       career,
       experience,
-<<<<<<< HEAD
-      selectedAreas,
-=======
       languages: selectedLanguages,
       instagram: instagram || "",
       linkedin: linkedin || "",
->>>>>>> main
     });
   };
 
@@ -101,41 +55,6 @@ const SignupStep7 = ({ navigation }) => {
       source={require("../../../../assets/background.png")}
       style={backgroundStyle.background}
     >
-<<<<<<< HEAD
-      <View style={styles.container}>
-        <Image 
-          source={require("../../../../assets/orchid.png")} 
-          style={styles.orchidImage} 
-        />
-        
-        <Animated.View style={[styles.content, { opacity: fadeAnim }]}>
-          <Text style={[fontStyle.h2, fontStyle.light, {marginTop:-50}]}>¿Cuál es el área de tu interés?</Text>
-
-          <View style={styles.areasContainer}>
-            {areas.map((area) => (
-              <TouchableOpacity
-                key={area}
-                style={[
-                  styles.areaButton,
-                  selectedAreas.includes(area) && styles.selectedAreaButton,
-                ]}
-                onPress={() => handleAreaSelect(area)}
-              >
-                <Text 
-                  style={[
-                    styles.areaButtonText,
-                    selectedAreas.includes(area) && styles.selectedAreaButtonText
-                  ]}
-                >
-                  {area}
-                </Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-        </Animated.View>
-
-        <View style={styles.navigationContainer}>
-=======
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View
         style={[
@@ -164,7 +83,6 @@ const SignupStep7 = ({ navigation }) => {
         </View>
         
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
->>>>>>> main
           <GoBackButton onPress={() => navigation.goBack()} />
           <NextButton onPress={handleContinue} />
         </View>
@@ -176,70 +94,4 @@ const SignupStep7 = ({ navigation }) => {
   );
 };
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  orchidImage: {
-    width: 80,
-    height: 80,
-    resizeMode: 'contain',
-    alignSelf: 'center', 
-    marginTop: 120,      
-  },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: "bold",
-    color: "white",
-    textAlign: "center",
-    marginBottom: 50,
-  },
-  areasContainer: {
-    flexDirection: 'row',
-    flexWrap: "wrap",
-    gap: 10,
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  navigationContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-
-  areaButton: {
-    backgroundColor: 'white',
-    borderRadius: 30,
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    width: '48%',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  selectedAreaButton: {
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    borderWidth: 3,
-    borderColor: "#007AFF",
-  },
-  areaButtonText: {
-    fontSize: 20,
-    fontWeight: '500',
-  },
-  buttonSubtitle: {
-    fontSize: 14,
-  },
-});
-
 export default SignupStep7;
-=======
-export default SignupStep7;
->>>>>>> main
