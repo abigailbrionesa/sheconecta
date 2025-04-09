6335abcf45711a7520a1481669efe25606748012
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 14:13:59 -0500

    fix profile better ui

--- app/features/account/screens/Perfil.js
+++ app/features/account/screens/Perfil.js
@@ -3,67 +3,54 @@ import {
   View,
   Text,
   Button,
-  StyleSheet,
+  TextInput,
+  ActivityIndicator,
   Image,
-  TouchableOpacity,
+  ImageBackground,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { FIREBASE_AUTH, FIREBASE_DB } from "../../../../FirebaseConfig";
-import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
-import { fontStyle } from "../../../utils/fontStyle";
-import { ImageBackground } from "react-native";
-import { backgroundStyle } from "../../../utils/backgroundStyle";
-import GoBackButton from "../../welcome/components/GoBackButton";
-import NextButton from "../../welcome/components/NextButton";
-import { uiStyle } from "../../../utils/uiStyle";
-import { getDoc } from "firebase/firestore";
-import { setDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
+import { Ionicons } from "@expo/vector-icons";
+import { FIREBASE_AUTH } from "../../../../FirebaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { FIREBASE_DB } from "../../../../FirebaseConfig";
+import { fontStyle } from "../../../utils/fontStyle";
+import { uiStyle } from "../../../utils/uiStyle";
+import { backgroundStyle } from "../../../utils/backgroundStyle";
+import Button1 from "../../welcome/components/Button1";
 
 export default function Perfil() {
   const user = FIREBASE_AUTH.currentUser;
-  const [nombre, setNombre] = useState("");
-  const [descripcion, setDescripcion] = useState("");
   const [editando, setEditando] = useState(false);
-
   const [userData, setUserData] = useState({
     firstName: "",
     lastName: "",
-    birthDate: "",
+    email: "",
+    profilePictureUrl: "",
     career: "",
     city: "",
     experienceLevel: "",
     interestAreas: [],
-    profilePictureUrl: "",
-    score: 0,
     university: "",
     socialLinks: {},
+    score: 0,
   });
+
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
+      setLoading(true);
       const userDoc = doc(FIREBASE_DB, "users", user.uid);
       const docSnap = await getDoc(userDoc);
       if (docSnap.exists()) {
         const data = docSnap.data();
-        setNombre(data.name || "");
-        setDescripcion(data.descripcion || "");
         setUserData({
-          firstName: data.firstName || "",
-          lastName: data.lastName || "",
-          birthDate: data.birthDate || "",
-          career: data.career || "",
-          city: data.city || "",
-          experienceLevel: data.experienceLevel || "",
-          interestAreas: data.interestAreas || [],
-          profilePictureUrl: data.profilePictureUrl || "",
-          score: data.score || 0,
-          university: data.university || "",
-          socialLinks: data.socialLinks || {},
+          ...data,
+          email: user.email,
         });
       }
+      setLoading(false);
     };
-
     fetchUserData();
   }, []);
 
@@ -75,102 +62,150 @@ export default function Perfil() {
     }
   };
 
-  const handleGuardar = async () => {
-    const userDoc = doc(FIREBASE_DB, "usuarios", user.uid);
-    await setDoc(
-      userDoc,
-      {
-        nombre,
-        descripcion,
-      },
-      { merge: true }
-    );
-
-    setEditando(false);
+  const handleSave = async () => {
+    setLoading(true);
+    const userDoc = doc(FIREBASE_DB, "users", user.uid);
+    try {
+      await setDoc(userDoc, userData, { merge: true });
+      setEditando(false);
+    } catch (error) {
+      console.error("Error saving profile:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const handleInputChange = (field, value) => {
+    setUserData((prevState) => ({ ...prevState, [field]: value }));
+  };
+
+  if (loading) {
+    return (
+      <View style={uiStyle.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
     <ImageBackground
       source={require("../../../../assets/background.png")}
       style={backgroundStyle.background}
     >
-      <View style={{ alignItems: "center", marginTop: 80,  }}>
+      <View
+        style={{ alignItems: "center", marginTop: 50, paddingHorizontal: 20 }}
+      >
         <Image
           source={{ uri: userData.profilePictureUrl }}
-          style={{ width: 100, height: 100, borderRadius: 50 }}
+          style={{
+            width: 120,
+            height: 120,
+            borderRadius: 60,
+            marginBottom: 20,
+          }}
         />
-<Button title="Cerrar sesión" onPress={handleLogout} color="red" />
+
 
         <View
           style={{
             backgroundColor: "white",
             padding: 20,
-            width: "100%",
             borderRadius: 10,
             alignItems: "center",
+            width: "100%",
+            gap:15,
           }}
         >
-          <Text style={[fontStyle.h3, fontStyle.dark]}>
+
+          <View style={{gap:5, alignItems:"center"}}>
+          <Text style={[fontStyle.h2, fontStyle.dark]}>
             {userData.firstName} {userData.lastName}
           </Text>
-
-          <Text style={[fontStyle.p, fontStyle.dark]}>{user.email}</Text>
-
+          <Text style={[fontStyle.p, fontStyle.dark]}>{userData.email}</Text>
+          </View>
           <Text style={[fontStyle.h4, fontStyle.pink]}>
-            Puntuación: {userData.score}{" "}
+            Puntuación: {userData.score}
           </Text>
 
-          <View style={{backgroundColor: "#e6dafd", width: "100%", padding:20,}}>
+          <View
+            style={{
+              backgroundColor: "#f1e9ff",
+              padding: 15,
+              borderRadius: 8,
+              marginTop: 20,
+              width: "100%",
+            }}
+          >
+            <Text style={[fontStyle.p, fontStyle.dark]}>
+              Carrera: {userData.career}
+            </Text>
+            <Text style={[fontStyle.p, fontStyle.dark]}>
+              Años de experiencia: {userData.experience}
+            </Text>
+            <Text style={[fontStyle.p, fontStyle.dark]}>
+              Áreas de interés:{" "}
+              {userData.interestAreas.length > 0 ? (
+                userData.interestAreas.map((area, index) => (
+                  <Text key={index}>{area} - </Text>
+                ))
+              ) : (
+                <Text>No especificadas</Text>
+              )}
+            </Text>
 
-            <Text style={[fontStyle.h4, fontStyle.gray]}>Carrera:{userData.career}</Text>
+            <Text style={[fontStyle.p, fontStyle.dark]}>
+              Universidad: {userData.university}
+            </Text>
 
-            <Text style={[fontStyle.h4, fontStyle.gray]}>{userData.city}</Text>
-
-            <Text style={fontStyle.h3}>Nivel de experiencia:</Text>
-            <Text>{userData.experienceLevel}</Text>
-
-            <Text style={fontStyle.h3}>Áreas de interés:</Text>
-            {userData.interestAreas.length > 0 ? (
-              userData.interestAreas.map((area, index) => (
-                <Text key={index}>{area}</Text>
-              ))
-            ) : (
-              <Text>No specified interest areas</Text>
-            )}
-
-            <Text style={fontStyle.h3}>Universidad:</Text>
-            <Text>{userData.university}</Text>
-
-            <Text style={fontStyle.h3}>Redes sociales:</Text>
             {userData.socialLinks.instagram ? (
-              <Text>Instagram: {userData.socialLinks.instagram}</Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Ionicons name="logo-instagram" size={20} color="#E4405F" />
+                <Text style={[fontStyle.p, fontStyle.gray, { marginLeft: 8 }]}>
+                  {userData.socialLinks.instagram}
+                </Text>
+              </View>
             ) : (
-              <Text>Instagram: No disponible</Text>
+              <Text></Text>
             )}
+
             {userData.socialLinks.linkedin ? (
-              <Text>LinkedIn: {userData.socialLinks.linkedin}</Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Ionicons name="logo-linkedin" size={20} color="#0077B5" />
+                <Text style={[fontStyle.p, fontStyle.gray, { marginLeft: 8 }]}>
+                  {userData.socialLinks.linkedin}
+                </Text>
+              </View>
             ) : (
-              <Text>LinkedIn: No disponible</Text>
-            )}
-
-            <Text style={fontStyle.h3}>Foto de perfil:</Text>
-
-            <Text style={fontStyle.h3}>Nombre:</Text>
-            {editando ? (
-              <TextInput
-                value={nombre}
-                onChangeText={setNombre}
-                style={fontStyle.h3}
-              />
-            ) : (
-              <Text>{nombre}</Text>
+              <Text></Text>
             )}
           </View>
 
-          <Button
-            title={editando ? "Guardar cambios" : "Editar perfil"}
-            onPress={editando ? handleGuardar : () => setEditando(true)}
-          />
+
+            <View style={{flexDirection:"row", justifyContent:"space-between", width:"100%",}}>
+          <Button1
+          onPress={handleLogout}
+          icon={<Ionicons name="log-out-outline" size={20} color="white" />}
+          color="#ab4769"
+        >
+          Cerrar Sesión
+        </Button1>
+
+          <Button1
+            onPress={editando ? handleSave : () => setEditando(true)}
+            icon={
+              <Ionicons
+                name={editando ? "save-outline" : "create-outline"}
+                size={20}
+                color="white"
+              />
+            }
+            color="#769ad4"
+          >
+            {editando ? "Guardar cambios" : "Editar perfil"}
+          </Button1>
+          </View>
+
+
         </View>
       </View>
     </ImageBackground>
