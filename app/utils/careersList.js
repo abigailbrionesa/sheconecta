72930cf97784c36273a175585c17fed148c39980
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 10:23:04 -0500

    careers list

---
+++ app/utils/careersList.js
@@ -0,0 +1,8 @@
+
+ export const careers = [
+    { label: "Ingeniería de Sistemas", value: "Ingeniería de Sistemas" },
+    { label: "Medicina", value: "Medicina" },
+    { label: "Abogacía", value: "Abogacía" },
+    { label: "Arquitectura", value: "Arquitectura" },
+    { label: "Otro", value: "Otro" },
+  ];
