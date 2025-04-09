fd78c74e8c2f3e9adfee8ff001e169f156d88707
dynngz <dayanagzrd@gmail.com>
2025-04-08 22:54:28 -0500

    Resolver conflictos en App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -23,10 +22,8 @@ import Experience from "./app/features/welcome/screens/Experience";
 import { ProfileProvider } from "./app/features/welcome/screens/ProfileContext";
 
 const Stack = createNativeStackNavigator();
-=======
 import Navigation from "./app/infraestructure/Navigation";
-import { useFonts } from "expo-font";
->>>>>>> main
+//import { useFonts } from "expo-font";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -37,71 +34,69 @@ export default function App() {
     "AbhayaLibre-SemiBold": require("./assets/fonts/AbhayaLibre-SemiBold.ttf"),
   });
 
-<<<<<<< HEAD
-  const [user, setUser] = useState(null);
-  const [initializing, setInitializing] = useState(true);
-  const [isNewUser, setIsNewUser] = useState(false);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
-      // Si el usuario cambia de null a un valor (sign up/login)
-      if (!user && currentUser) {
-        // Si el usuario acaba de registrarse, marcarlo como nuevo
-        const creationTime = new Date(currentUser.metadata.creationTime);
-        const lastSignInTime = new Date(currentUser.metadata.lastSignInTime);
+  // const [user, setUser] = useState(null);
+  // const [initializing, setInitializing] = useState(true);
+  // const [isNewUser, setIsNewUser] = useState(false);
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
+  //     // Si el usuario cambia de null a un valor (sign up/login)
+  //     if (!user && currentUser) {
+  //       // Si el usuario acaba de registrarse, marcarlo como nuevo
+  //       const creationTime = new Date(currentUser.metadata.creationTime);
+  //       const lastSignInTime = new Date(currentUser.metadata.lastSignInTime);
         
-        // Si la diferencia es menor a 10, asumimos que es un usuario nuevo
-        setIsNewUser(lastSignInTime - creationTime < 10000);
-      }
+  //       // Si la diferencia es menor a 10, asumimos que es un usuario nuevo
+  //       setIsNewUser(lastSignInTime - creationTime < 10000);
+  //     }
       
-      setUser(currentUser);
-      if (initializing) setInitializing(false);
-    });
-    return unsubscribe;
-  }, [user]);
+  //     setUser(currentUser);
+  //     if (initializing) setInitializing(false);
+  //   });
+  //   return unsubscribe;
+  // }, [user]);
 
-  if (!fontsLoaded || initializing) {
-=======
+  // if (!fontsLoaded || initializing) {
+
   if (!fontsLoaded) {
->>>>>>> main
     return null;
   }
 
   return (
-<<<<<<< HEAD
-    <NavigationContainer>
-      <ProfileProvider>
-        <Stack.Navigator initialRouteName={user ? (isNewUser ? "ProfileInfo" : "HomeScreen") : "Welcome"}>
-          {/* Rutas de autenticación */}
-          {!user && (
-            <>
-              <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-              <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-              <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
-            </>
-          )}
+
+    // <NavigationContainer>
+    //   <ProfileProvider>
+    //     <Stack.Navigator initialRouteName={user ? (isNewUser ? "ProfileInfo" : "HomeScreen") : "Welcome"}>
+    //       {/* Rutas de autenticación */}
+    //       {!user && (
+    //         <>
+    //           <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+    //           <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+    //           <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+    //         </>
+    //       )}
           
-          {/* Rutas para usuarios autenticados */}
-          {user && (
-            <>
-              <Stack.Screen name="Profileopc" component={Profileopc} options={{ headerShown: false }} />
-              <Stack.Screen name="ExperienceLevel" component={ExperienceLevel} options={{ headerShown: false }} />
-              <Stack.Screen name="ProfileInfo" component={ProfileInfo} options={{ headerShown: false }} />
-              <Stack.Screen name="InterestArea" component={InterestArea} options={{ headerShown: false }} />
-              <Stack.Screen name="UserBio" component={Bio} options={{ headerShown: false }} />
-              <Stack.Screen name="ProfilePhoto" component={ProfilePhoto} options={{ headerShown: false }} />
-              <Stack.Screen name="Experience" component={Experience} options={{ headerShown: false }} />
-              <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-            </>
-          )}
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </ProfileProvider>
-    </NavigationContainer>
-=======
+    //       {/* Rutas para usuarios autenticados */}
+    //       {user && (
+    //         <>
+    //           <Stack.Screen name="Profileopc" component={Profileopc} options={{ headerShown: false }} />
+    //           <Stack.Screen name="ExperienceLevel" component={ExperienceLevel} options={{ headerShown: false }} />
+    //           <Stack.Screen name="ProfileInfo" component={ProfileInfo} options={{ headerShown: false }} />
+    //           <Stack.Screen name="InterestArea" component={InterestArea} options={{ headerShown: false }} />
+    //           <Stack.Screen name="UserBio" component={Bio} options={{ headerShown: false }} />
+    //           <Stack.Screen name="ProfilePhoto" component={ProfilePhoto} options={{ headerShown: false }} />
+    //           <Stack.Screen name="Experience" component={Experience} options={{ headerShown: false }} />
+    //           <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+    //         </>
+    //       )}
+    //     </Stack.Navigator>
+    //     <StatusBar style="auto" />
+    //   </ProfileProvider>
+    // </NavigationContainer>
     <>
       <Navigation />
     </>
->>>>>>> main
+
   );
 }
