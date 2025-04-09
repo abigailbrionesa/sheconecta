c6df885505385106d20aa83c05c84965d8b175e7
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 10:47:37 -0500

    connect to real list of universities

--- app/features/welcome/screens/SignupStep5.js
+++ app/features/welcome/screens/SignupStep5.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Alert, ScrollView, Button } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { uiStyle } from "../../../utils/uiStyle";
@@ -8,29 +8,45 @@ import { fontStyle } from "../../../utils/fontStyle";
 import NextButton from "../components/NextButton";
 import GoBackButton from "../components/GoBackButton";
 import DropDownPicker from "react-native-dropdown-picker";
-import { public_universities } from "../../../utils/universitiesList";
-import { private_universities } from "../../../utils/universitiesList";
+import {
+  public_universities,
+  private_universities,
+} from "../../../utils/universitiesList";
 import { careers } from "../../../utils/careersList";
-
+import Button1 from "../components/Button1";
 const SignupStep5 = ({ navigation }) => {
   const route = useRoute();
-  const [university, setUniversity] = useState("");
-  const [career, setCareer] = useState("");
-  const [experience, setExperience] = useState("");
   const [universityType, setUniversityType] = useState(null);
+  const [university, setUniversity] = useState(null);
+  const [career, setCareer] = useState(null);
+  const [experience, setExperience] = useState("");
 
-  const { email, password, firstName, lastName, birthDate, role, departamento, provincia } = route.params;
+  const [uniOpen, setUniOpen] = useState(false);
+  const [careerOpen, setCareerOpen] = useState(false);
+
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    birthDate,
+    role,
+    departamento,
+    provincia,
+  } = route.params;
 
   const validateCityUniversityCareer = () => {
-    if (!city || !university || !career) {
-      Alert.alert("Error", "Please complete the university, and career fields.");
+    if (!university || !career) {
+      Alert.alert(
+        "Error",
+        "Por favor, completa los campos de universidad y carrera."
+      );
       return false;
     }
     return true;
   };
 
   const goToStep6 = () => {
-    console.log(university)
     if (validateCityUniversityCareer()) {
       navigation.navigate("SignupStep6", {
         email,
@@ -48,6 +64,15 @@ const SignupStep5 = ({ navigation }) => {
     }
   };
 
+  const formattedUniversities =
+    universityType === "privada"
+      ? private_universities.map((item) => ({ ...item, key: item.value }))
+      : public_universities.map((item) => ({ ...item, key: item.value }));
+
+  const formattedCareers = careers.map((item) => ({
+    ...item,
+    key: item.value,
+  }));
 
   return (
     <ImageBackground
@@ -62,57 +87,83 @@ const SignupStep5 = ({ navigation }) => {
           ]}
         >
           <View style={{ gap: 15 }}>
-            <Text style={[fontStyle.h2, fontStyle.light]}>Cuéntanos un poco más sobre ti</Text>
+            <Text style={[fontStyle.h2, fontStyle.light]}>
+              Cuéntanos un poco más sobre ti
+            </Text>
+            <Text style={[fontStyle.h3, fontStyle.light]}>Universidad:</Text>
 
             <View style={{ flexDirection: "row", gap: 15 }}>
-              <Button
-                title="Universidad Privada"
-                onPress={() => setUniversityType("privada")}
-              />
-              <Button
-                title="Universidad Pública"
-                onPress={() => setUniversityType("publica")}
-              />
+              <Button1
+                onPress={() => {
+                  setUniversityType("privada");
+                  setUniversity(null);
+                  setCareer(null);
+                }}
+              >
+                Privada
+              </Button1>
+              <Button1
+                onPress={() => {
+                  setUniversityType("publica");
+                  setUniversity(null);
+                  setCareer(null);
+                }}
+              >
+                Pública
+              </Button1>
             </View>
 
             {universityType && (
               <>
                 <Text style={[fontStyle.h3, fontStyle.light]}>Universidad</Text>
                 <DropDownPicker
-                  open={true}
+                  open={uniOpen}
+                  setOpen={setUniOpen}
                   value={university}
-                  items={
-                    universityType === "privada"
-                      ? private_universities
-                      : public_universities
-                  }
                   setValue={setUniversity}
+                  items={formattedUniversities}
                   style={uiStyle.input}
                   placeholder="Selecciona tu universidad"
+                  zIndex={3000}
+                  zIndexInverse={1000}
                 />
               </>
             )}
 
-            <Text style={[fontStyle.h3, fontStyle.light]}>Carrera</Text>
-            <DropDownPicker
-              open={true}
-              value={career}
-              items={careers}
-              setValue={setCareer}
-              style={uiStyle.input}
-              placeholder="Selecciona tu carrera"
-            />
+            {university && (
+              <>
+                <Text style={[fontStyle.h3, fontStyle.light]}>Carrera</Text>
+                <DropDownPicker
+                  open={careerOpen}
+                  setOpen={setCareerOpen}
+                  value={career}
+                  setValue={setCareer}
+                  items={formattedCareers}
+                  style={uiStyle.input}
+                  placeholder="Selecciona tu carrera"
+                  zIndex={2000}
+                  zIndexInverse={2000}
+                />
+              </>
+            )}
 
-            <Text style={[fontStyle.h3, fontStyle.light]}>Años de experiencia</Text>
-            <TextInput
-              style={uiStyle.input}
-              value={experience}
-              placeholder="Años de experiencia"
-              keyboardType="numeric"
-              onChangeText={setExperience}
-            />
-
-            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            {career && (
+              <>
+                <Text style={[fontStyle.h3, fontStyle.light]}>
+                  Años de experiencia
+                </Text>
+                <TextInput
+                  style={uiStyle.input}
+                  value={experience}
+                  placeholder="Años de experiencia"
+                  keyboardType="numeric"
+                  onChangeText={setExperience}
+                />{" "}
+              </>
+            )}
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
               <GoBackButton onPress={() => navigation.goBack()} />
               <NextButton onPress={goToStep6} />
             </View>
