b44ed043b2433f45d0ce00ff817c96118c635063
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 19:59:54 -0500

    mostrar details de los retos

--- app/features/challenges/screens/ChallengeDetails.js
+++ app/features/challenges/screens/ChallengeDetails.js
@@ -1,8 +1,25 @@
-import { Text } from "react-native";
-export const ChallengeDetails = () => {
+import React from "react";
+import { View, Text } from "react-native";
+import { ImageBackground } from "react-native";
+import { backgroundStyle } from "../../../utils/backgroundStyle";
+import { fontStyle } from "../../../utils/fontStyle";
+import Button1 from "../../welcome/components/Button1"
+export const ChallengeDetails = ({ route }) => {
+  const { challenge } = route.params;
   return (
-    <View>
-      <Text>Challenge Details</Text>
-    </View>
+    <ImageBackground
+      source={require("../../../../assets/background.png")}
+      style={backgroundStyle.background}
+    >
+      <View style={{ padding: 20 }}>
+        <Text style={[fontStyle.h3]}>{challenge.nombre}</Text>
+        <Text style={[fontStyle.p]}>Categoria: {challenge.categoria}</Text>
+        <Text style={[fontStyle.p]}>Objetivo: {challenge.objetivo}</Text>
+        <Text style={[fontStyle.p]}>Tarea: {challenge.tarea}</Text>
+        <Text style={[fontStyle.p]}>Validación: {challenge.validacion}</Text>
+        <Text style={[fontStyle.p]}>Puntos: {challenge.puntos} puntos</Text>
+        <Button1>Validar</Button1>
+      </View>
+    </ImageBackground>
   );
-}
+};
