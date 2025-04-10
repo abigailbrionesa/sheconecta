ae3f7aa27b1c3d7ba719fb54fe2607263c75f151
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 20:01:23 -0500

    add retos

--- app/infraestructure/navigation/MainTabNavigator.js
+++ app/infraestructure/navigation/MainTabNavigator.js
@@ -4,6 +4,7 @@ import { OpportunitiesNavigator } from './OpportunitiesNavigator';
 import { CommunityNavigator } from './CommunityNavigator';
 import { MessageNavigator} from './MessageNavigator';
 import Perfil from '../../features/account/screens/Perfil';
+import { ChallengesNavigator } from './ChallengesNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ export default function MainTabNavigator() {
     <Tab.Navigator initialRouteName="Oportunidades" screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Oportunidades" component={OpportunitiesNavigator} />
       <Tab.Screen name="Conectar" component={CommunityNavigator} />
+      <Tab.Screen name="Retos" component={ChallengesNavigator} />
       <Tab.Screen name="Chat" component={MessageNavigator} />
       <Tab.Screen name="Perfil" component={Perfil} />
     </Tab.Navigator>
