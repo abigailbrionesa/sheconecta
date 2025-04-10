6a3112a34ee4527849ef984be2412f5acfea9ba1
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-10 07:03:55 -0500

    validate data

--- app/features/connect/components/UserProfileInfo.js
+++ app/features/connect/components/UserProfileInfo.js
@@ -20,24 +20,47 @@ export default function UserProfileInfo({ user }) {
           gap: 3,
         }}
       >
-        <Image
-          source={{ uri: user.profilePictureUrl }}
-          style={{
-            width: 160,
-            height: 160,
-            borderRadius: 80,
-            marginBottom: 20,
-          }}
-        />
-        <Text style={[fontStyle.h2, fontStyle.pink]}>
-          {user.firstName} {user.lastName}
-        </Text>
-        <Text style={[fontStyle.p, fontStyle.darker]}>{user.career}</Text>
-        <Text style={[fontStyle.p, fontStyle.darker]}>{user.university}</Text>
-        <Text style={[fontStyle.p, fontStyle.darker]}>Experiencia: {user.yearsExperience} años</Text>
-        <Text style={[fontStyle.p, fontStyle.darker]}>{user.location.provincia}, {user.location.departamento}</Text>
-        <Text style={[fontStyle.p, fontStyle.darker]}>Intereses: {user.interestAreas?.join(", ") || "N/A"}
-        </Text>
+        {user.profilePictureUrl && (
+          <Image
+            source={{ uri: user.profilePictureUrl }}
+            style={{
+              width: 160,
+              height: 160,
+              borderRadius: 80,
+              marginBottom: 20,
+            }}
+          />
+        )}
+
+        {user.type && <Text style={[fontStyle.h3, fontStyle.darker]}>{user.type}</Text>}
+
+        {user.firstName && user.lastName && (
+          <Text style={[fontStyle.h2, fontStyle.pink]}>
+            {user.firstName} {user.lastName}
+          </Text>
+        )}
+
+        {user.career && <Text style={[fontStyle.p, fontStyle.darker]}>{user.career}</Text>}
+
+        {user.university && <Text style={[fontStyle.p, fontStyle.darker]}>{user.university}</Text>}
+
+        {user.yearsExperience && (
+          <Text style={[fontStyle.p, fontStyle.darker]}>
+            Experiencia: {user.yearsExperience} años
+          </Text>
+        )}
+
+        {user.location?.provincia && user.location?.departamento && (
+          <Text style={[fontStyle.p, fontStyle.darker]}>
+            {user.location.provincia}, {user.location.departamento}
+          </Text>
+        )}
+
+        {user.interestAreas && user.interestAreas.length > 0 && (
+          <Text style={[fontStyle.p, fontStyle.darker]}>
+            Intereses: {user.interestAreas.join(", ")}
+          </Text>
+        )}
       </View>
     </View>
   );
