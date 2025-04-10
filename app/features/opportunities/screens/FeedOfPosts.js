a00c1b52a8d53b28dfa82c6505d1fc59f181f6c9
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-10 06:52:44 -0500

    feat feed of posts

--- app/features/opportunities/screens/FeedOfPosts.js
+++ app/features/opportunities/screens/FeedOfPosts.js
@@ -2,68 +2,70 @@ import React, { useEffect, useState, useCallback } from "react";
 import {
   View,
   Text,
-  FlatList,
   ImageBackground,
-  StyleSheet,
   TouchableOpacity,
   ScrollView,
+  FlatList,
+  Image,
 } from "react-native";
-import { FIREBASE_AUTH, FIREBASE_DB } from "../../../../FirebaseConfig";
+import { FIREBASE_DB } from "../../../../FirebaseConfig";
 import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { backgroundStyle } from "../../../utils/backgroundStyle";
-import { uiStyle } from "../../../utils/uiStyle";
 import Button1 from "../../welcome/components/Button1";
 import { fontStyle } from "../../../utils/fontStyle";
-import { Image } from "react-native";
-
-const typeOptions = ["curso", "proyecto", "experiencia", "beca"];
-const tagOptions = ["Ingenieria", "Matematica", "Tecnología", "Ciencia"];
+import Loading from "../../welcome/components/Loading";
+import { typeOptions } from "../../../utils/typeOptions";
+import { tagOptions } from "../../../utils/tagOptions";
 
 const FeedOfPosts = () => {
   const [publications, setPublications] = useState([]);
   const [selectedTypes, setSelectedTypes] = useState([]);
   const [selectedTags, setSelectedTags] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
-
-  const fetchAllPublications = async () => {
-    try {
-      const usersRef = collection(FIREBASE_DB, "users");
-      const usersSnapshot = await getDocs(usersRef);
-      const allPublications = [];
-
-      for (const userDoc of usersSnapshot.docs) {
-        const userId = userDoc.id;
-        const userDocRef = doc(FIREBASE_DB, "users", userId);
-        const userSnapshot = await getDoc(userDocRef);
-        const userData = userSnapshot.data();
-
-        const publicationsRef = collection(
-          FIREBASE_DB,
-          "users",
-          userId,
-          "publications"
-        );
-        const publicationsSnapshot = await getDocs(publicationsRef);
-
-        publicationsSnapshot.forEach((pubDoc) => {
-          allPublications.push({
-            userId,
-            publicationId: pubDoc.id,
-            author: `${userData.firstName} ${userData.lastName}`,
-            ...pubDoc.data(),
-          });
-        });
-      }
-
-      setPublications(allPublications);
-    } catch (error) {
-      console.error("Error fetching publications:", error);
-    }
-  };
 
   useFocusEffect(
     useCallback(() => {
+      const fetchAllPublications = async () => {
+        setLoading(true);
+        try {
+          const usersRef = collection(FIREBASE_DB, "users");
+          const usersSnapshot = await getDocs(usersRef);
+          const allPublications = [];
+
+          for (const userDoc of usersSnapshot.docs) {
+            const userId = userDoc.id;
+            const userDocRef = doc(FIREBASE_DB, "users", userId);
+            const userSnapshot = await getDoc(userDocRef);
+            const userData = userSnapshot.data();
+
+            const publicationsRef = collection(
+              FIREBASE_DB,
+              "users",
+              userId,
+              "publications"
+            );
+            const publicationsSnapshot = await getDocs(publicationsRef);
+
+            publicationsSnapshot.forEach((pubDoc) => {
+              allPublications.push({
+                userId,
+                publicationId: pubDoc.id,
+                author: `${userData.firstName} ${userData.lastName}`,
+                ...pubDoc.data(),
+              });
+            });
+          }
+
+          setPublications(allPublications);
+        } catch (error) {
+          console.error("Error fetching publications:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
       fetchAllPublications();
     }, [])
   );
@@ -86,131 +88,72 @@ const FeedOfPosts = () => {
     return acc;
   }, {});
 
-  const tagOptions = [
-    {
-      name: "Ingeniería",
-      imageUrl:
-        "https://www.itcsa.es/wp-content/uploads/Ingenieria-planos-detalle-320x220.jpg",
-    },
-    {
-      name: "Matemática",
-      imageUrl:
-        "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/164EE/production/_109347319_gettyimages-611195980.jpg.webp",
-    },
-    {
-      name: "Tecnología",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuMFJkwKYntOfm07r0IDZgK1arpN4mV3jmeg&s",
-    },
-    {
-      name: "Ciencia",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx5Y97nIfyLzsWAyyQLrPxQjs-hjFokujppA&s",
-    },
-  ];
+  const renderTagButton = (tag) => (
+    <TouchableOpacity
+      key={tag.name}
+      onPress={() => toggleTag(tag.name)}
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        margin: 5,
+        opacity: selectedTags.includes(tag.name) ? 0.6 : 1,
+      }}
+    >
+      <Image source={{ uri: tag.imageUrl }} style={{ width: 60, height: 60, borderRadius: 30 }} />
+      <Text style={[fontStyle.p, fontStyle.light]}>{tag.name}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderPublications = (type) => (
+    <View key={type} style={{ backgroundColor: "#faf5f3", flex: 1, padding: 20, gap: 15 }}>
+      <Text style={[fontStyle.h3, fontStyle.pink]}>{type}</Text>
+      {groupedByType[type].map((item) => (
+        <View key={item.publicationId} style={{ backgroundColor: "#e6dafd", padding: 10, borderRadius: 10 }}>
+          <Text style={fontStyle.h4}>{item.title}</Text>
+          <Text style={fontStyle.p}>Autor: {item.author}</Text>
+          <View style={{ backgroundColor: "gray", width: "100%", height: 1 }} />
+          <Text style={fontStyle.p}>{item.description}</Text>
+          <Text style={[fontStyle.p, fontStyle.gray]}>
+            {new Date(item.createdAt.seconds * 1000).toLocaleDateString()}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
 
   return (
     <ImageBackground
       source={require("../../../../assets/background.png")}
       style={backgroundStyle.background}
     >
-      <ScrollView contentContainerStyle={{ }}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
         <View style={{ padding: 20, gap: 15 }}>
-
-
-          <View>
-            <Text style={[fontStyle.h3, fontStyle.light]}>
-              Filtrar por tema
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                flexWrap: "wrap",
-                marginTop: 10,
-                justifyContent: "space-between",
-              }}
-            >
-              {tagOptions.map((tag) => (
-                <TouchableOpacity
-                  key={tag.name}
-                  onPress={() => toggleTag(tag.name)}
-                  style={[
-                    {
-                      justifyContent: "center",
-                      alignItems: "center",
-                      width: 80,
-                      height: 80,
-                      borderRadius: 40,
-                      margin: 5,
-                    },
-                    selectedTags.includes(tag.name) && { opacity: 0.6 },
-                  ]}
-                >
-                  <Image
-                    source={{ uri: tag.imageUrl }}
-                    style={{ width: 60, height: 60, borderRadius: 30 }}
-                  />
-                  <Text style={[fontStyle.p, fontStyle.light]}>{tag.name}</Text>
-                </TouchableOpacity>
-
-                
-              ))}
-            </View>
+          <Text style={[fontStyle.h3, fontStyle.light]}>Filtrar por tema</Text>
+          <View style={{ flexDirection: "row", flexWrap: "wrap", marginTop: 10, justifyContent: "space-between" }}>
+            {tagOptions.map(renderTagButton)}
           </View>
-        </View>
 
-        <View style={{ backgroundColor: "#faf5f3", flex:1 }}>
-          {typeOptions.map(
-            (type) =>
-              groupedByType[type].length > 0 && (
-                <View
-                  key={type}
-                  style={{ backgroundColor: "#faf5f3", flex:1, padding: 20, gap: 15 }}
-                >
-                  <Text style={[fontStyle.h3, fontStyle.pink]}>{type}</Text>
-
-                  {groupedByType[type].map((item) => (
-                    <View
-                      key={item.publicationId}
-                      style={{
-                        backgroundColor: "#e6dafd",
-                        padding: 10,
-                        borderRadius: 10,
-                      }}
-                    >
-                      <Text style={fontStyle.h4}>{item.title}</Text>
-                      <Text style={fontStyle.p}>Autor: {item.author}</Text>
-                      <View
-                        style={{
-                          backgroundColor: "gray",
-                          width: "100%",
-                          height: 1,
-                        }}
-                      />
-                      <Text style={fontStyle.p}>{item.description}</Text>
-                      <Text style={[fontStyle.p, fontStyle.gray]}>
-                        {new Date(
-                          item.createdAt.seconds * 1000
-                        ).toLocaleDateString()}
-                      </Text>
-                    </View>
-                  ))}
-
-
-
-                </View>
-              )
+          {loading ? (
+            <Loading />
+          ) : (
+            <View>
+              {typeOptions.map(
+                (type) =>
+                  groupedByType[type].length > 0 && renderPublications(type)
+              )}
+            </View>
           )}
         </View>
       </ScrollView>
 
-          <View style={{margin:20}}>
-          <Button1 onPress={() => navigation.navigate("CreatePost")}>
-            Crear Publicación
-      </Button1>
-          </View>
-
-
+      <View style={{ margin: 20 }}>
+        <Button1 onPress={() => navigation.navigate("CreatePost")}>
+          Crear Publicación
+        </Button1>
+      </View>
     </ImageBackground>
   );
 };
