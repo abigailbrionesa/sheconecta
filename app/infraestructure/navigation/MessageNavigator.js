2e4a5260d1686cdce700c1f40713c1968355645d
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-09 20:19:50 -0500

    fix

--- app/infraestructure/navigation/MessageNavigator.js
+++ app/infraestructure/navigation/MessageNavigator.js
@@ -3,14 +3,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MessagesInbox from '../../features/chat/screens/MessagesInbox';
 import PersonalChat from '../../features/chat/screens/PersonalChat';
 
-
 const Stack = createNativeStackNavigator();
 
 export const MessageNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="MessagesInbox" 
-    screenOptions={{ headerShown: false }}>
-        
+    <Stack.Navigator initialRouteName="MessagesInbox" screenOptions={{ headerShown: false }}>
       <Stack.Screen 
         name="ChatScreen" 
         component={PersonalChat} 
