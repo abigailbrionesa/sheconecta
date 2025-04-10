99305c8c8292479c8dc7f3f5e95fbe2d3dcbf344
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-10 08:05:24 -0500

    style both headers

--- app/infraestructure/navigation/MainTabNavigator.js
+++ app/infraestructure/navigation/MainTabNavigator.js
@@ -2,20 +2,72 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { OpportunitiesNavigator } from './OpportunitiesNavigator';
 import { CommunityNavigator } from './CommunityNavigator';
-import { MessageNavigator} from './MessageNavigator';
+import { MessageNavigator } from './MessageNavigator';
 import Perfil from '../../features/account/screens/Perfil';
 import { ChallengesNavigator } from './ChallengesNavigator';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
+const tabBarIcon = (iconName) => ({ color, size }) => (
+  <Ionicons name={iconName} size={size} color={color} />
+);
+
 export default function MainTabNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Oportunidades" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Oportunidades" component={OpportunitiesNavigator} />
-      <Tab.Screen name="Conectar" component={CommunityNavigator} />
-      <Tab.Screen name="Retos" component={ChallengesNavigator} />
-      <Tab.Screen name="Chat" component={MessageNavigator} />
-      <Tab.Screen name="Perfil" component={Perfil} />
+    <Tab.Navigator
+      initialRouteName="Oportunidades"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#9eb0dc',
+          borderTopWidth: 0, 
+          height: 60,
+        },
+        tabBarActiveTintColor: '#3f60a0',
+        tabBarInactiveTintColor: '#f0f7fd', 
+        tabBarLabelStyle: {
+          fontSize: 12, 
+          fontFamily: 'AbhayaLibre-ExtraBold',
+
+        },
+      }}>
+      <Tab.Screen
+        name="Oportunidades"
+        component={OpportunitiesNavigator}
+        options={{
+          tabBarIcon: tabBarIcon('rocket-outline'),
+          title:"Posts"
+        }}
+      />
+      <Tab.Screen
+        name="Conectar"
+        component={CommunityNavigator}
+        options={{
+          tabBarIcon: tabBarIcon('people-outline'),
+        }}
+      />
+      <Tab.Screen
+        name="Retos"
+        component={ChallengesNavigator}
+        options={{
+          tabBarIcon: tabBarIcon('trophy-outline'),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={MessageNavigator}
+        options={{
+          tabBarIcon: tabBarIcon('chatbubbles-outline'),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarIcon: tabBarIcon('person-outline'),
+        }}
+      />
     </Tab.Navigator>
   );
 }
