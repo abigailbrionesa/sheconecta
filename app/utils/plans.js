5c8d490dc4e775963069c0cbc2211e52a7317d90
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-10 21:41:25 -0500

    refactor

---
+++ app/utils/plans.js
@@ -0,0 +1,46 @@
+
+export const plans = [
+  {
+    name: "Plan Free (Predeterminado)",
+    price: "Gratis",
+    features: [
+      "Mentorías: 2/mes",
+      "Crear 1 proyecto, unirse a 2",
+      "Comunidad básica (principiante/intermedio)",
+      "1 taller mensual (según cupo)",
+      "Participación en retos (máx. 2 por semana)",
+      "Puntos x1",
+    ],
+  },
+  {
+    name: "Conecta+",
+    price: "S/ 15",
+    features: [
+      "Mentorías ilimitadas",
+      "Proyectos ilimitados",
+      "Acceso total a comunidad",
+      "Acceso a talleres",
+      "Participación en retos",
+      "Recursos exclusivos, sorteos, eventos",
+      "Multiplicador de puntos x1.2",
+    ],
+  },
+  {
+    name: "SheTeam",
+    price: "S/ 45",
+    features: [
+      "Todo el Plan Conecta+",
+      "Acceso para 4 personas",
+      "Canal privado, seguimiento grupal, retos grupales",
+    ],
+  },
+  {
+    name: "ConectaPro",
+    price: "S/ 120 anual",
+    features: [
+      "Todo lo anterior + bonificación inicial de 200 pts",
+      "Badge exclusivo + puntos x1.5",
+      "Acceso a sorteos anuales grandes",
+    ],
+  },
+];
